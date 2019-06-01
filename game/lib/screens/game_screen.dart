ce60fd22d31083bb316918b2edf8992ad347f235
Luan Nico <luanpotter27@gmail.com>
2019-06-01 12:10:18 -0300

    .

--- game/lib/screens/game_screen.dart
+++ game/lib/screens/game_screen.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../util.dart';
+
 class GameScreen extends StatelessWidget {
   const GameScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('game'),
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(color: Colors.cyan),
+          ),
+        ),
+        Container(
+          child: pad(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                    decoration: BoxDecoration(color: Colors.red),
+                    width: 100,
+                    height: 100),
+                Container(
+                    decoration: BoxDecoration(color: Colors.red),
+                    width: 100,
+                    height: 100),
+                Container(
+                    decoration: BoxDecoration(color: Colors.red),
+                    width: 100,
+                    height: 100),
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
