ede6dcc15831a699ab897d0f580ba5d7c2a1a735
Luan Nico <luanpotter27@gmail.com>
2019-06-01 12:02:08 -0300

    .

---
+++ game/lib/game.dart
@@ -0,0 +1,3 @@
+import 'package:flame/game.dart';
+
+class Game extends BaseGame {}
---
+++ game/lib/screens/game_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('game'),
+    );
+  }
+}
