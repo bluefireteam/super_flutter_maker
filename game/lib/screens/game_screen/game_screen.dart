8299429bfea84d16f6729c2b7775aac29b4517e5
Luan Nico <luanpotter27@gmail.com>
2019-06-01 12:44:59 -0300

    .

--- game/lib/screens/game_screen/game_screen.dart
+++ game/lib/screens/game_screen/game_screen.dart
@@ -3,9 +3,29 @@ import 'package:flutter/material.dart';
 import '../../util.dart';
 import 'game_widget_widget.dart';
 
+const List<String> COMPONENTS = [
+  'Text',
+  'Center',
+  'Row',
+  'Column',
+];
 
 class GameScreen extends StatelessWidget {
   const GameScreen({Key key}) : super(key: key);
+
+  Widget slider(BuildContext context) {
+    return Container(
+      height: 132,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Container(width: 8.0),
+          ...COMPONENTS.map((t) => GameWidgetWidget(text: t)).toList(),
+          Container(width: 8.0),
+        ], 
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +36,7 @@ class GameScreen extends StatelessWidget {
             decoration: BoxDecoration(color: Colors.cyan),
           ),
         ),
-        Container(
-          child: pad(
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                GameWidgetWidget(),
-                GameWidgetWidget(),
-                GameWidgetWidget(),
-              ],
-            ),
-          ),
-        )
+        slider(context),
       ],
     );
   }
