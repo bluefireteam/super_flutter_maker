9416d81f99f728e40312f808b0e0d79397f3c399
Luan Nico <luanpotter27@gmail.com>
2019-06-01 14:22:15 -0300

    .

--- game/lib/screens/game_screen/builder_view.dart
+++ game/lib/screens/game_screen/builder_view.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../../models/challenge_widget.dart';
+
 class BuilderView extends StatelessWidget {
+  Widget slider(BuildContext context) {
+    return Container(
+      height: 132,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          Container(width: 8.0),
+          ...ChallengeWidget.all(),
+          Container(width: 8.0),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.cyan,      
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            color: Colors.cyan,
+          ),
+        ),
+        slider(context),
+      ],
     );
   }
 }
--- game/lib/screens/game_screen/game_screen.dart
+++ game/lib/screens/game_screen/game_screen.dart
@@ -1,33 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
-import '../../models/challenge_widget.dart';
-import 'game_view.dart';
+import 'builder_view.dart';
+import 'preview_view.dart';
+import 'target_view.dart';
 
-class GameScreen extends StatelessWidget {
+const SLIDER_COUNT = 3;
+
+const TextStyle buttonsStyle = TextStyle(color: Colors.white, fontSize: 20);
+
+class GameScreen extends StatefulWidget {
   const GameScreen({Key key}) : super(key: key);
 
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  final SwiperController swiperController = SwiperController();
+
   Widget slider(BuildContext context) {
-    return Container(
-      height: 132,
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          Container(width: 8.0),
-          ...ChallengeWidget.all(),
-          Container(width: 8.0),
-        ],
-      ),
+    return Swiper(
+      itemBuilder: (BuildContext context, int index) {
+        switch (index) {
+          case 0: return TargetView();
+          case 1: return BuilderView();
+          case 2: return PreviewView();
+          default: return Text('Loading...');
+        }
+      },
+      itemCount: SLIDER_COUNT,
+      loop: false,
+      pagination: null,
+      control: null,
+      controller: swiperController,
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        Expanded(
-          child: GameView(),
-        ),
         slider(context),
+        Positioned(
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                FlatButton(
+                  child: Text('Target', style: buttonsStyle),
+                  onPressed: () => swiperController.move(0),
+                ),
+                FlatButton(
+                  child: Text('Builder', style: buttonsStyle),
+                  onPressed: () => swiperController.move(1),
+                ),
+                FlatButton(
+                  child: Text('Preview', style: buttonsStyle),
+                  onPressed: () => swiperController.move(2),
+                ),
+              ],
+            ),
+          ),
+          top: 16.0,
+          left: 0.0,
+          right: 0.0,
+        ),
       ],
     );
   }
