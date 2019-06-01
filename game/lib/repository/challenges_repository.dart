5203b7e09271d7cec4cebf17ad21264d48c59253
Pedro Zaroni <pzaroni.dextra@vstsalelo.onmicrosoft.com>
2019-06-01 17:22:13 -0300

    Add differend goal for intermediate challenge

--- game/lib/repository/challenges_repository.dart
+++ game/lib/repository/challenges_repository.dart
@@ -1,6 +1,8 @@
 import '../models/challenge.dart';
 import '../models/widgets/center.dart';
 import '../models/widgets/text.dart';
+import '../models/widgets/column.dart';
+import '../models/widgets/raised_button.dart';
 
 
 class ChallengeRepository {
@@ -12,19 +14,34 @@ class ChallengeRepository {
     helloWorldChallenge.title = "Welcome to Flutter";
     helloWorldChallenge.difficultyLevel = 1;
 
+    helloWorldChallenge.child =
+      CenterWidget()
+        ..setPropertyValue('child', TextWidget()..setPropertyValue('text', 'Hello World'));
+
     Challenge helloWorldChallenge2 = Challenge();
     helloWorldChallenge2.id = "2";
     helloWorldChallenge2.title = "Intermediate Challenge";
     helloWorldChallenge2.difficultyLevel = 2;
+
+    helloWorldChallenge2.child =
+      CenterWidget()
+        ..setPropertyValue(
+          'child',
+          ColumnWidget()
+            ..setPropertyValue(
+              'children',
+              [
+                TextWidget()..setPropertyValue('text', 'First item'),
+                TextWidget()..setPropertyValue('text', 'Second item'),
+              ]    
+            )
+        );
 
     Challenge helloWorldChallenge3 = Challenge();
     helloWorldChallenge3.id = "3";
     helloWorldChallenge3.title = "Expert Challenge";
     helloWorldChallenge3.difficultyLevel = 3;
 
-    helloWorldChallenge.child = helloWorldChallenge2.child = helloWorldChallenge3.child =
-      CenterWidget()
-        ..setPropertyValue('child', TextWidget()..setPropertyValue('text', 'Hello World'));
 
     _challenges = [helloWorldChallenge, helloWorldChallenge2, helloWorldChallenge3,];
   }
